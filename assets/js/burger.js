708ec38427e02592c437416ba797072f1855c320
N1TAXE <n1t4x3@gmail.com>
2024-02-16 16:10:57 +0100

    Fixes

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const burgerMenu = document.querySelector(".burger-menu");
-    const headerNav = document.querySelector(".header-nav");
+    const headerNav = document.querySelector(".mobile-menu");
     burgerMenu.addEventListener("click", function() {
-        headerNav.classList.toggle("show");
+        headerNav.classList.toggle('show')
+        if (headerNav.style.maxHeight) {
+            headerNav.style.maxHeight = null;
+            headerNav.style.background = 'transparent';
+        } else {
+            headerNav.style.maxHeight = headerNav.scrollHeight * 2 + "px";
+            headerNav.style.background = '#1c2021';
+        }
     });
 });
